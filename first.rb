58ce973a2f65528ea5329e113e3d82ca82bfa39b
Elisa Wang <elisa.wang@iss.nthu.edu.tw>
2016-10-15 13:13:51 +0800

    get some features of a specific asteroid and the title of a daily picture

--- first.rb
+++ first.rb
@@ -17,6 +17,15 @@ today_neos =  JSON.parse(today_response.to_s)['near_earth_objects']
 results[:today_count] = today_count
 results[:today_neos] = today_neos
 
+# Get the name and estimated diameter (for example) of a specific Asteroid based on its id
+asteroid_id = '3542519'
+asteroid_response = HTTP.get(URI.join('https://api.nasa.gov/neo/rest/v1/neo/', "#{asteroid_id}"), params: { api_key: api_token })
+nasa_response[:asteroid_info] = asteroid_response
+asteroid_name = JSON.parse(asteroid_response.to_s)['name']
+diameter = JSON.parse(asteroid_response.to_s)['estimated_diameter']
+results[:asteroid_name] = asteroid_name
+results[:estimated_diameter] = diameter
+
 # Get NASA status
 status_response = HTTP.get('https://api.nasa.gov/neo/rest/v1/stats', params: { api_key: api_token })
 nasa_response[:status] = status_response
@@ -27,8 +36,10 @@ results[:update_date] = update_date
 apod_response = HTTP.get('https://api.nasa.gov/planetary/apod', params: { api_key: api_token })
 nasa_response[:apod] = apod_response
 daily_pic = JSON.parse(apod_response.to_s)['hdurl']
+pic_title = JSON.parse(apod_response.to_s)['title']
 pic_explanation = JSON.parse(apod_response.to_s)['explanation']
 results[:daily_pic] = daily_pic
+results[:pic_title] = pic_title
 results[:pic_explanation] = pic_explanation
 
 # Write File
